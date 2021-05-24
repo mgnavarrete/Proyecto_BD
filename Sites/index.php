3a234e515442c601d41c1df000d6b19383c83608
Menta <mgnavarrete@uc.cl>
2021-05-24 19:21:30 -0400

    index

---
+++ Sites/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title> CONSULTA DE DATOS </title>
+</head>
+
+<body>
+Hola Mundo
+</body>
+
+</html>
