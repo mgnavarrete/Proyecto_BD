c184d8b2b46ba872b701466a241e13b4e6649a06
Menta <mgnavarrete@uc.cl>
2021-05-24 19:59:37 -0400

    test index

--- Sites/index.php
+++ Sites/index.php
@@ -6,7 +6,57 @@
 </head>
 
 <body>
-Hola Mundo
+<h1>
+    Consulta de Datos
+</h1>
+<p>Estoy probando como consultar datos!</p>
 </body>
 
+
+
+<table>
+    <tr>
+        <td>Tiendas</td>
+        <td>Id</td>
+    </tr>
+
+    <tr>
+        <td>GameStop</td>
+        <td>1</td>
+    </tr>
+
+</table>
+
+<?php
+
+# Variable
+$var = 10;
+$boolean = true;
+$msj = "Wena Menta";
+
+# Mostrar vista
+
+echo "Entero: $var, Boolean: $boolean, Mensaje: $msj";
+echo "<br>";
+# Array
+$array1 = array(1,2,3,4);
+$array2 = array("platano" => "amarillo", "frutilla" => "roja");
+
+print_r($array1);
+
+echo "<br>";
+
+# foreach
+
+foreach ($array2 as $fruta => $color){
+    echo "<p>$fruta es de color: $color </p>";
+}
+
+print_r($array2);
+
+?>
+
+
+
+</body>
 </html>
