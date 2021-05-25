49082c5582951aa76be98b700c9e188954365d5f
mgnavarrete <mgnavarrete@uc.cl>
2021-05-24 23:13:55 -0400

    asdfasfas

---
+++ Sites/consultas/consulta_tienda_pro_tipo.php
@@ -0,0 +1,32 @@
+<?php include('../templates/header.html');   ?>
+
+<body>
+
+  <?php
+  require("../config/conexion.php"); #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  $tipo_producto = array("No comestibles" => "no_comestibles", "Congelados" => "congelados", "Frescos" => "frescos", "Conserva" => "conserva");
+  $key = $_POST["tipo"];
+  $producto = $tipo_producto[$key];
+  $query = "";
+  $result = $db -> prepare($query);
+  $result -> execute();
+  $dataCollected = $result -> fetchAll(); #Obtiene todos los resultados de la consulta en forma de un arreglo
+  ?>
+
+  <table>
+    <tr>
+      <th>ID</th>
+      <th>Nombre</th>
+      <th>Altura</th>
+      <th>Peso</th>
+      <th>Exp Base</th>
+      <th>Tipo</th>
+    </tr>
+  <?php
+  foreach ($dataCollected as $p) {
+    echo "<tr> <td>$p[0]</td> <td>$p[1]</td> <td>$p[2]</td> <td>$p[3]</td> <td>$p[4]</td> <td>$p[5]</td> </tr>";
+  }
+  ?>
+  </table>
+
+<?php include('../templates/footer.html'); ?>
--- Sites/index.php
+++ Sites/index.php
@@ -53,48 +53,18 @@
   <br>
 
 
-  <h3 align="center"> ¿Quieres buscar un Pokemón por su ID?</h3>
+  <h3 align="center">¿Quieres buscar la tienda que tenga algun tipo de producto?</h3>
 
-  <form align="center" action="consultas/consulta_stats.php" method="post">
-    Id:
-    <input type="text" name="id_elegido">
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
   
-  <br>
-  <br>
-  <br>
 
-  <h3 align="center"> ¿Quieres conocer los Pokemones más altos que: ?</h3>
-
-  <form align="center" action="consultas/consulta_altura.php" method="post">
-    Altura Mínima:
-    <input type="text" name="altura">
-    <br/><br/>
-    <input type="submit" value="Buscar">
-  </form>
-  <br>
-  <br>
-  <br>
-
-  <h3 align="center">¿Quieres buscar todos los pokemones por tipo?</h3>
-
-  <?php
-  #Primero obtenemos todos los tipos de pokemones
-  require("config/conexion.php");
-  $result = $db -> prepare("SELECT DISTINCT tipo FROM pokemones;");
-  $result -> execute();
-  $dataCollected = $result -> fetchAll();
-  ?>
-
-  <form align="center" action="consultas/consulta_tipo.php" method="post">
+  <form align="center" action="consultas/consulta_tienda_pro_tipo.php" method="post">
     Seleccinar un tipo:
     <select name="tipo">
       <?php
       #Para cada tipo agregamos el tag <option value=value_of_param> visible_value </option>
-      foreach ($dataCollected as $d) {
-        echo "<option value=$d[0]>$d[0]</option>";
+      $tipo_producto = array("No comestibles" => "no_comestibles", "Congelados" => "congelados", "Frescos" => "frescos", "Conserva" => "conserva");
+      foreach ($tipo_producto as $key => $value) {
+        echo "<option value=$key>$key</option>";
       }
       ?>
     </select>
