25db027a7c319de045e363382fc60668bb3e8699
Menta <mgnavarrete@uc.cl>
2021-05-24 20:47:36 -0400

    Primera consulta

--- Sites/consultas/consulta_tiendas_despacho.php
+++ Sites/consultas/consulta_tiendas_despacho.php
@@ -6,24 +6,20 @@
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   require("../config/conexion.php");
 
-	$tipo = $_POST["tipo_elegido"];
-	$nombre = $_POST["nombre_pokemon"];
-
- 	$query = "SELECT pid,nombre, tipo FROM pokemones WHERE tipo LIKE '%$tipo%' AND nombre LIKE '%$nombre%';";
+ 	$query = "SELECT * FROM tiendas_despacho;";
 	$result = $db -> prepare($query);
 	$result -> execute();
-	$pokemones = $result -> fetchAll();
+	$tiendas = $result -> fetchAll();
   ?>
 
 	<table>
     <tr>
-      <th>ID</th>
       <th>Nombre</th>
-      <th>Tipo</th>
+      <th>Comuna</th>
     </tr>
   <?php
-	foreach ($pokemones as $pokemon) {
-  		echo "<tr> <td>$pokemon[0]</td> <td>$pokemon[1]</td> <td>$pokemon[2]</td> </tr>";
+	foreach ($tiendas as $tienda) {
+  		echo "<tr> <td>$tienda[0]</td> <td>$tienda[1]</td> </tr>";
 	}
   ?>
 	</table>
