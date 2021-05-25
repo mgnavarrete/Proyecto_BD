9fc9d5dbc6df716bfc1d917387beb9d2d5ca20dd
mgnavarrete <mgnavarrete@uc.cl>
2021-05-24 23:18:57 -0400

    dfas

--- Sites/consultas/consulta_tienda_pro_tipo.php
+++ Sites/consultas/consulta_tienda_pro_tipo.php
@@ -1,12 +1,13 @@
 <?php include('../templates/header.html');   ?>
 
 <body>
-
+<h1 align="center">Disponibilidad de tipo de productos </h1>
   <?php
   require("../config/conexion.php"); #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   $tipo_producto = array("No comestibles" => "no_comestibles", "Congelados" => "congelados", "Frescos" => "frescos", "Conserva" => "conserva");
   $key = $_POST["tipo"];
   $producto = $tipo_producto[$key];
+  printf($key);
   print_r($producto);
   $query = "";
   $result = $db -> prepare($query);
@@ -14,7 +15,7 @@
   $dataCollected = $result -> fetchAll(); #Obtiene todos los resultados de la consulta en forma de un arreglo
   ?>
 
-  <table>
+  <!-- <table>
     <tr>
       <th>ID</th>
       <th>Nombre</th>
@@ -28,6 +29,6 @@
     echo "<tr> <td>$p[0]</td> <td>$p[1]</td> <td>$p[2]</td> <td>$p[3]</td> <td>$p[4]</td> <td>$p[5]</td> </tr>";
   }
   ?>
-  </table>
+  </table> -->
 
 <?php include('../templates/footer.html'); ?>
