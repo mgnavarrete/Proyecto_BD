90c8d602c7a6d8f33daa2fe6381e4811d370b9e5
mgnavarrete <mgnavarrete@uc.cl>
2021-05-26 11:37:33 -0400

    ss

--- Sites/consultas/consulta_descripcion.php
+++ Sites/consultas/consulta_descripcion.php
@@ -9,7 +9,7 @@
 
 	$descripcion = strtolower($_POST["descripcion"]);
 	echo "$descripcion";
- 	$query = "SELECT usuarios.id_usuario usuarios.nombre FROM usuarios, compras, productos WHERE productos.descripcion LIKE '%$descripcion%' AND usuarios.id_usuario = compras.id_usuario AND produtos.id_producto = compras.id_producto;";
+ 	$query = "SELECT usuarios.id_usuario, usuarios.nombre FROM usuarios, compras, productos WHERE productos.descripcion LIKE '%$descripcion%' AND usuarios.id_usuario = compras.id_usuario AND produtos.id_producto = compras.id_producto;";
 	$result = $db -> prepare($query);
 	$result -> execute();
 	$usuarios = $result -> fetchAll();
