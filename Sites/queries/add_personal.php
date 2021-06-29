7576ccacc9f6f47ae356fedd8fedbdbdb3cf26fc
mgnavarrete <mgnavarrete@uc.cl>
2021-06-29 12:15:40 -0400

    🐛

--- Sites/queries/add_personal.php
+++ Sites/queries/add_personal.php
@@ -13,27 +13,44 @@
 	$result -> execute();
 	$personal = $result -> fetchAll();
 
+	?>
 
+<section class="hero is-info is-fullheight">
+  <div class="hero-body">
+    <div class="container has-text-centered">
+    <p class="title">
+		menta.mp3
+    </p>
 
+<table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth" align="center">
+    <tr>
+      <th>rut1</th>
+	  <th>rut2</th>
+      <th>direccion</th>
+ 
+    </tr>
 
+<?php
 	foreach ($personal as $user) {
-		$query2 = "SELECT id_direccion FROM tiendas, personal WHERE personal.rut = '$user[1]' AND personal.id_tienda = tiendas.id_tienda;";
+		$query2 = "SELECT personal.rut, tiendas.id_direccion FROM tiendas, personal WHERE personal.rut = '$user[1]' AND personal.id_tienda = tiendas.id_tienda;";
 		$result2 = $db2 -> prepare($query2);
 		$result2 -> execute();
 		$id_direccion = $result2 -> fetchAll();
 
-		echo "$id_direccion[0]";
+		echo "<tr> <td>$user[1]</td> <td>$id_direccion[0]</td> <td>$id_direccion[1]</td></tr>";
 
 	}
-
-
-
-	
 ?>
 
 
 
+</table>
 
+
+
+</div>
+</div>
+</section>
 
 
 <?php include('../templates/footer_bulma.html');   ?> 
