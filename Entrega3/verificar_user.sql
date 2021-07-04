2d7dd01e105a3b2467bc952338983b18efdba542
mgnavarrete <mgnavarrete@uc.cl>
2021-07-03 20:51:58 -0400

    🎨

--- Entrega3/verificar_user.sql
+++ Entrega3/verificar_user.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION
 
 -- declaramos la función y sus argumentos
-verificar_user(rut varchar(100), contraseña varchar(100))
+verificar_user(ruts varchar(100), contraseña varchar(100))
 
 -- declaramos lo que retorna 
 RETURNS BOOLEAN AS $$
@@ -10,11 +10,11 @@ RETURNS BOOLEAN AS $$
 -- definimos nuestra función
 BEGIN
 
-    IF rut NOT IN (SELECT usuarios.rut FROM usuarios) THEN
+    IF ruts NOT IN (SELECT usuarios.rut FROM usuarios) THEN
         RETURN FALSE;
     END IF;
 
-    IF contraseña != (SELECT usuarios.password FROM usuarios WHERE usuarios.rut = rut) THEN
+    IF contraseña != (SELECT usuarios.password FROM usuarios WHERE usuarios.rut = ruts) THEN
         RETURN FALSE;
 
     ELSE 
