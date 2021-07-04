6946ad0a310f34a536c93c746b888bb0c78d2147
mgnavarrete <mgnavarrete@uc.cl>
2021-07-04 19:26:59 -0400

    verificar pass

---
+++ Entrega3/verificar_pass.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE FUNCTION
+
+-- declaramos la función y sus argumentos
+new_pass(id_user int, old_pass varchar(100), new_pass varchar(100))
+
+-- declaramos lo que retorna 
+RETURNS BOOLEAN AS $$
+
+
+
+-- definimos nuestra función
+BEGIN
+
+    IF old_pass != (SELECT password FROM usuarios WHERE id_user = id_usuarios) THEN
+    RETURN FALSE;
+
+    IF new_pass == '' THEN
+    RETURN FALSE;
+
+    ELSE
+    RETURN TRUE;
+
+    END IF;
+
+
+END
+$$ language plpgsql
