9cbce7057343c2b4796ad1fd5c595f6906f1c770
mgnavarrete <mgnavarrete@uc.cl>
2021-07-06 02:18:37 -0400

    🎨

--- Entrega3/add_direccion_new.sql
+++ Entrega3/add_direccion_new.sql
@@ -15,7 +15,7 @@ BEGIN
         
         SELECT INTO id_dir MAX(id_direccion)+1 FROM direcciones;
         INSERT INTO direcciones values (is_dir, nombre, comuna);
-        INSERT INTO usuario_direccion values (id_user, id_dire);
+        INSERT INTO usuario_direccion values (id_user, id_dir);
         RETURN TRUE;
     
 
